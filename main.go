f41fc517d82e0377f39152272a34cd5ae3ca802c
kouta1612 <kouta1612world69@gmail.com>
2022-04-23 10:57:07 +0900

    import package

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"go_practice/ch2/practice1"
+)
+
+func main() {
+	fmt.Println(practice1.CToK(0))
+	fmt.Println(practice1.KToC(0))
+	fmt.Println(practice1.KToF(0))
+}
