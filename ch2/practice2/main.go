9aa99e8eb5f3bb52f6d146cd76bfc22333eda38b
kouta1612 <kouta1612world69@gmail.com>
2022-04-23 12:28:26 +0900

    practice2

---
+++ ch2/practice2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"go_practice/ch2/tempconv"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args[1:]) == 0 {
+		var t float64
+		fmt.Print("温度を数値で入力してください：")
+		fmt.Scan(&t)
+		tempPrint(t)
+	}
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		tempPrint(t)
+	}
+}
+
+func tempPrint(t float64) {
+	f := tempconv.Fahrenheit(t)
+	c := tempconv.Celsius(t)
+	fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+}
