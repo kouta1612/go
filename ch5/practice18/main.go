af7c69ae7bbf84aac16ce33cb29acdd59a262373
kouta1612 <kouta1612world69@gmail.com>
2022-05-10 19:19:10 +0900

    practice18

---
+++ ch5/practice18/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		filename, num, err := fetch(url)
+		if err != nil {
+			log.Fatalf("fetch error: %v", err)
+		}
+		fmt.Printf("%s(row: %d) is created.\n", filename, num)
+	}
+}
+
+func fetch(url string) (fileName string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	defer func() {
+		if closeErr := f.Close(); err == nil {
+			err = closeErr
+		}
+	}()
+	n, err = io.Copy(f, resp.Body)
+	return local, n, err
+}
