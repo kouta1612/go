8c27d26b3c219e8fd5b6b165c4243b2948a884dd
kouta1612 <kouta1612world69@gmail.com>
2023-05-06 13:10:47 +0900

    リファクタリング

--- ch7/http3/http3.go
+++ ch7/http3/http3.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	mux := http.NewServeMux()
-	mux.Handle("/list", http.HandlerFunc(db.list))
-	mux.Handle("/price", http.HandlerFunc(db.price))
+	mux.HandleFunc("/list", db.list)
+	mux.HandleFunc("/price", db.price)
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
 
