86f2a446cd612944c7fe70a58009b5e3a82f0ef1
kouta1612 <kouta1612world69@gmail.com>
2023-05-14 12:20:41 +0900

    Mutexを使って相互排他を実現

--- ch9/main.go
+++ ch9/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"go_practice/ch9/bank1"
 	"go_practice/ch9/bank2"
+	"go_practice/ch9/bank3"
 	"time"
 )
 
 func main() {
+	// bank1
 	go func() {
 		bank1.Deposit(200)
 		fmt.Println("=", bank1.Balance())
@@ -17,12 +19,23 @@ func main() {
 
 	time.Sleep(500 * time.Microsecond)
 
+	// bank2
 	go func() {
 		bank2.Deposit(200)
 		fmt.Println("=", bank2.Balance())
 	}()
 
 	go bank2.Balance()
+
+	time.Sleep(500 * time.Microsecond)
+
+	// bank3
+	go func() {
+		bank3.Deposit(200)
+		fmt.Println("=", bank3.Balance())
+	}()
+
+	go bank3.Balance()
 
 	time.Sleep(500 * time.Microsecond)
 }
