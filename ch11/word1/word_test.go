44e89717884dcc1ec79e769f7d8cc7d812ba52ea
kouta1612 <kouta1612world69@gmail.com>
2023-05-15 21:40:17 +0900

    失敗するテストを追加

--- ch11/word1/word_test.go
+++ ch11/word1/word_test.go
@@ -11,6 +11,19 @@ func TestPalindrome(t *testing.T) {
 	}
 }
 
+func TestFrenchPalindrome(t *testing.T) {
+	if !IsParindrome("été") {
+		t.Error(`IsParindrome("été") = false`)
+	}
+}
+
+func TestCanalPalindrome(t *testing.T) {
+	input := "A man, a plan, a canal: Panama"
+	if !IsParindrome(input) {
+		t.Errorf(`IsParindrome(%q) = false`, input)
+	}
+}
+
 func TestNonPalindrome(t *testing.T) {
 	if IsParindrome("palindrome") {
 		t.Error(`IsParindrome("palindrome") = true`)
