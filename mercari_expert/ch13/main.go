0b17f4d279b75f849b5dc8851a921ee8b5dbbed2
kouta1612 <kouta1612world69@gmail.com>
2023-05-18 22:35:08 +0900

    追記

--- mercari_expert/ch13/main.go
+++ mercari_expert/ch13/main.go
@@ -19,4 +19,7 @@ func main() {
 	fmt.Println(buf.String())
 
 	io.Copy(os.Stdout, os.Stdin)
+	// 以下の出力は標準入力を閉じたときにだけ出力される
+	// ctrl+Dで閉じたら出力されるがctrl+Cで閉じても出力されずにプログラムが終わる
+	fmt.Println("end of file!")
 }
