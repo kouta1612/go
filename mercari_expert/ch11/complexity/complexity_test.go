4f2028ecb7279d884ba4ae2158ff313d74ca0a02
kouta1612 <kouta1612world69@gmail.com>
2023-05-18 13:14:03 +0900

    抽象構文木を使って循環複雑度をテスト（Countは仮で実装した）

---
+++ mercari_expert/ch11/complexity/complexity_test.go
@@ -0,0 +1,61 @@
+package complexity
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
+)
+
+func TestComplexity(t *testing.T) {
+	testcases := []struct {
+		name       string
+		code       string
+		complexity int
+	}{
+		// テストケースをここに追加していく
+		{
+			name: "simple function",
+			code: `
+				package main
+				func Double(n int) int {
+					return n * 2
+				}
+			`,
+			complexity: 1,
+		},
+	}
+
+	for _, testtestcase := range testcases {
+		t.Run(testtestcase.name, func(t *testing.T) {
+			a := GetAST(t, testtestcase.code)
+			c := Count(a)
+			if c != testtestcase.complexity {
+				t.Errorf("got=%d, want=%d", c, testtestcase.complexity)
+			}
+		})
+	}
+}
+
+func Count(node ast.Node) int {
+	count := 1
+	return count
+}
+
+// 引用: https://github.com/knsh14/gocc/blob/9078b24a5eb4377455473212ec67b8034de1439f/complexity/complexity_test.go
+func GetAST(t *testing.T, code string) ast.Node {
+	t.Helper()
+
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", code, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, decl := range file.Decls {
+		if fd, ok := decl.(*ast.FuncDecl); ok {
+			return fd
+		}
+	}
+	t.Fatal("no function declear found")
+	return nil
+}
