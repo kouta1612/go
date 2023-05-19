7385f1f5a25f893d11cf5d510e6c408da6369d34
kouta1612 <kouta1612world69@gmail.com>
2023-05-19 13:55:32 +0900

    JANコード確認

--- mercari_expert/ch15/main.go
+++ mercari_expert/ch15/main.go
@@ -16,4 +16,14 @@ func main() {
 	code := seed + strconv.Itoa(digit)
 	fmt.Println(seed, digit, code)
 	fmt.Println(luhn.Verify(code))
+
+	jan13 := checkdigit.NewJAN13()
+	seed = "456995111617"
+	digit, err = jan13.Generate(seed)
+	if err != nil {
+		fmt.Println(err)
+	}
+	code = seed + strconv.Itoa(digit)
+	fmt.Println(seed, digit, code)
+	fmt.Println(jan13.Verify(code))
 }
